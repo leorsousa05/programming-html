51c3eff2f0be1ce1dad3887b0a407c27de7d0d28
leorsousa05 <leorsousa05@gmail.com>
2024-03-20 01:06:33 -0300

    If Statement Sanitizing

--- lib/conditional/index.ts
+++ lib/conditional/index.ts
@@ -1,53 +1,62 @@
-class ConditionalStatements {
-    checkStatement(statement: string): boolean {
-        return eval(statement);
-    }
+'use strict'
+
+export class ConditionalStatements {
+	checkStatement(statement: string): boolean {
+		const regex = /[^a-zA-Z0-9<>=>\s]/g;
+		if (regex.test(statement)) {
+			throw new Error("That's not a valid if statement!")
+		}
+
+		return eval(statement);
+	}
 }
 
 export class IfStatementElement extends HTMLElement {
 
-    constructor() {
-        super()
-    }
+	constructor() {
+		super()
+	}
 
-    static get observedAttributes() {
-        return ["value"]
-    }
+	static get observedAttributes() {
+		return ["value"]
+	}
 
-    connectedCallback() {
-        const conditional = this.attributes.getNamedItem("conditional")?.value;
-        const { checkStatement } = new ConditionalStatements();
+	connectedCallback() {
+		const conditional = this.attributes.getNamedItem("conditional")?.value;
+		const { checkStatement } = new ConditionalStatements();
 
-        if(!conditional) {
-            throw new Error("Conditional Missing");
-        }
+		if (!conditional) {
+			throw new Error("Conditional Missing");
+		}
 
-        const conditionalCheck = checkStatement(conditional);
-        
-        if(!conditionalCheck) {
-            this.childNodes.forEach(node => !node.nodeName.includes("ELSE-ELEMENT") ? node.replaceWith("") : false);
-            this.childNodes.forEach(node => console.log(node.nodeName))
-            return false;
-        }
+		const conditionalCheck = checkStatement(conditional);
+		console.log(conditionalCheck);
 
-        const attribute = document.createAttribute("conditionalResponse")
-        attribute.value = String(conditionalCheck);
+		if (!conditionalCheck) {
+			this.childNodes.forEach(node => !node.nodeName.includes("ELSE-ELEMENT") ? node.replaceWith("") : false);
+			this.childNodes.forEach(node => console.log(node.nodeName))
+			return false;
+		}
 
-        this.attributes.setNamedItem(attribute);
-    }
+		console.log("oi");
+		const attribute = document.createAttribute("conditionalResponse")
+		attribute.value = String(conditionalCheck);
+
+		this.attributes.setNamedItem(attribute);
+	}
 
 }
 
 export class ElseStatementElement extends HTMLElement {
-    constructor() {
-        super()
-    }
+	constructor() {
+		super()
+	}
 
-    connectedCallback() {
-        const conditionalResponse = this.parentElement?.attributes.getNamedItem("conditionalResponse")?.value;
-        if(conditionalResponse) {
-            this.innerHTML = "";
-        }
-    }
+	connectedCallback() {
+		const conditionalResponse = this.parentElement?.attributes.getNamedItem("conditionalResponse")?.value;
+		if (conditionalResponse) {
+			this.innerHTML = "";
+		}
+	}
 }
 
--- lib/loops/index.ts
+++ lib/loops/index.ts
@@ -1,64 +1,66 @@
-class LoopStatements {
-    forEachLoop(iter: [], nodeParent: Node, elementOutput?: string) {
-        iter.map(item => {
-            let element: HTMLElement = document.createElement("p");
-            if (elementOutput) {
-                element = document.createElement(elementOutput);
-            }
-            element.innerText = item;
-            nodeParent.appendChild(element);
-        })
-    }
+'use strict'
 
-    async forLoop(range: string, nodeParent: HTMLElement) {
-        const childs = nodeParent.childNodes;
-        const rangeArray: number[] = range.split("..").map(number => parseInt(number));
-        childs.forEach(node => {
-            for (let initial = rangeArray[0]; initial < rangeArray[1]; initial++) {
-                nodeParent.appendChild(node.cloneNode(true));
-            }
-        })
+export class LoopStatements {
+	forEachLoop(iter: [], nodeParent: Node, elementOutput?: string) {
+		iter.map(item => {
+			let element: HTMLElement = document.createElement("p");
+			if (elementOutput) {
+				element = document.createElement(elementOutput);
+			}
+			element.innerText = item;
+			nodeParent.appendChild(element);
+		})
+	}
 
-    }
+	async forLoop(range: string, nodeParent: HTMLElement) {
+		const childs = nodeParent.childNodes;
+		const rangeArray: number[] = range.split("..").map(number => parseInt(number));
+		childs.forEach(node => {
+			for (let initial = rangeArray[0]; initial < rangeArray[1]; initial++) {
+				nodeParent.appendChild(node.cloneNode(true));
+			}
+		})
+
+	}
 }
 
 export class ForEachLoopElement extends HTMLElement {
-    constructor() {
-        super()
-    }
+	constructor() {
+		super()
+	}
 
-    static get observedAttributes() {
-        return ["list", "element"];
-    }
+	static get observedAttributes() {
+		return ["list", "element"];
+	}
 
-    connectedCallback() {
-        const list = this.attributes.getNamedItem("list")?.value;
-        const elementOutput = this.attributes.getNamedItem("element")?.value;
-        const { forEachLoop } = new LoopStatements();
+	connectedCallback() {
+		const list = this.attributes.getNamedItem("list")?.value;
+		const elementOutput = this.attributes.getNamedItem("element")?.value;
+		const { forEachLoop } = new LoopStatements();
 
-        if (list) {
-            const listParsed: [] = eval(list);
-            forEachLoop(listParsed, this, elementOutput);
-        }
-    }
+		if (list) {
+			const listParsed: [] = eval(list);
+			forEachLoop(listParsed, this, elementOutput);
+		}
+	}
 
 }
 
 export class ForLoopElement extends HTMLElement {
-    constructor() {
-        super()
-    }
+	constructor() {
+		super()
+	}
 
-    static get observedAttributes() {
-        return ["range"]
-    }
+	static get observedAttributes() {
+		return ["range"]
+	}
 
-    connectedCallback() {
-        const range = this.attributes.getNamedItem("range")?.value;
-        const { forLoop } = new LoopStatements;
+	connectedCallback() {
+		const range = this.attributes.getNamedItem("range")?.value;
+		const { forLoop } = new LoopStatements;
 
-        if (range) {
-            forLoop(range, this);
-        }
-    }
+		if (range) {
+			forLoop(range, this);
+		}
+	}
 }
